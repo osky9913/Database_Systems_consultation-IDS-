a992fa4187775650019b3fe20c902356c85f05b1
osky9913 <m.osvald9913@gmail.com>
2020-03-22 15:11:00 +0100

    select_1 done

--- select_from_homework/select_1.sql
+++ select_from_homework/select_1.sql
@@ -3,25 +3,46 @@
 -- jméno a příjmení přítele, e-mail přítele).Uspořádejte podle přihlašovacího jména Jana
 --  Nováka.
 
--- SELECT "accountNumber" FROM "USER" WHERE "nameSurname" = 'Jan Novak');
+( Select "login_name1",  "name_1" , "login_name2" , "name_2","email" from
+(Select * From
+(
+Select "loginName" as "login_name1" , "nameSurname" as "name_1" , "accountNumber1" FROM
+    ( SELECT  * FROM "USER" WHERE "nameSurname" = 'Jan Novak' ) t1
+ inner join
+     ( SELECT "accountNumber2" , "accountNumber1"  FROM ASSOCIATED_ACCOUNTS ) t2
+ On t1."accountNumber" = t2."accountNumber2"
+ ) leftside
+ inner join
+(select "accountNumber", "nameSurname" as "name_2" ,"loginName" as "login_name2", "email"  from "USER" ) rightside
+On leftside."accountNumber1" = rightside."accountNumber" ) fulltable )
+
+UNION
+
+(Select "login_name1",  "name_1" , "login_name2","name_2" , "email" from
+(Select * From
+(
+Select"loginName" as "login_name1" , "nameSurname" as "name_1" , "accountNumber2" FROM
+    ( SELECT  * FROM "USER" WHERE "nameSurname" = 'Jan Novak' ) t1
+inner join
+    ( SELECT "accountNumber1" , "accountNumber2"  FROM ASSOCIATED_ACCOUNTS ) t2
+On t1."accountNumber" = t2."accountNumber1"
+) leftside
+
+inner join
+(select "accountNumber", "nameSurname" as "name_2" ,"loginName" as "login_name2", "email"  from "USER" ) rightside
+On leftside."accountNumber2" = rightside."accountNumber" ) fulltable)
 
 
 
 
 
--- SELECT "loginName" , "email" from User Where (
- --                                      Select "accountNumber2"
- --                                      from ASSOCIATED_ACCOUNTS
-  --                                     WHERE ASSOCIATED_ACCOUNTS."accountNumber1" =
-  --                                           (SELECT "accountNumber" FROM "USER" WHERE "nameSurname" = 'Jan Novak')
-
--- Select "accountNumber2" , "accountNumber1" from ASSOCIATED_ACCOUNTS WHERE
--- ASSOCIATED_ACCOUNTS."accountNumber1" =  (SELECT "accountNumber" FROM "USER" WHERE "nameSurname" = 'Jan Novak');
 
 
-Select * from "USER"(
-    Select "accountNumber2", "accountNumber1"
-    from ASSOCIATED_ACCOUNTS
-    WHERE ASSOCIATED_ACCOUNTS."accountNumber2" = (SELECT "accountNumber" FROM "USER" WHERE "nameSurname" = 'Jan Novak')
-) allData  join  allData."accountNumber2" = "USER"."accountNumber1";
+
+
+
+
+
+
+
 
