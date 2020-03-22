819cf5d0523b75554f3113f0012b474f2794781a
osky9913 <m.osvald9913@gmail.com>
2020-03-22 17:44:06 +0100

    select_1 redone done

--- select_from_homework/select_1.sql
+++ select_from_homework/select_1.sql
@@ -3,35 +3,22 @@
 -- jméno a příjmení přítele, e-mail přítele).Uspořádejte podle přihlašovacího jména Jana
 --  Nováka.
 
-( Select "login_name1",  "name_1" , "login_name2" , "name_2","email" from
-(Select * From
-(
-Select "loginName" as "login_name1" , "nameSurname" as "name_1" , "accountNumber1" FROM
-    ( SELECT  * FROM "USER" WHERE "nameSurname" = 'Jan Novak' ) t1
- inner join
-     ( SELECT "accountNumber2" , "accountNumber1"  FROM ASSOCIATED_ACCOUNTS ) t2
- On t1."accountNumber" = t2."accountNumber2"
- ) leftside
- inner join
+Select  "login_name1",  "name_1" , "login_name2" , "name_2","email" From
 (select "accountNumber", "nameSurname" as "name_2" ,"loginName" as "login_name2", "email"  from "USER" ) rightside
-On leftside."accountNumber1" = rightside."accountNumber" ) fulltable )
-
-UNION
-
-(Select "login_name1",  "name_1" , "login_name2","name_2" , "email" from
-(Select * From
-(
-Select"loginName" as "login_name1" , "nameSurname" as "name_1" , "accountNumber2" FROM
-    ( SELECT  * FROM "USER" WHERE "nameSurname" = 'Jan Novak' ) t1
-inner join
-    ( SELECT "accountNumber1" , "accountNumber2"  FROM ASSOCIATED_ACCOUNTS ) t2
-On t1."accountNumber" = t2."accountNumber1"
-) leftside
 
 inner join
-(select "accountNumber", "nameSurname" as "name_2" ,"loginName" as "login_name2", "email"  from "USER" ) rightside
-On leftside."accountNumber2" = rightside."accountNumber" ) fulltable)
 
+(Select * From
+    ( SELECT "loginName" as "login_name1" , "nameSurname" as "name_1" , "accountNumber" FROM "USER" WHERE "nameSurname" = 'Jan Novak' ) t1
+inner join
+    (
+        (SELECT "accountNumber2" as "Jan", "accountNumber1" as "Friend" FROM ASSOCIATED_ACCOUNTS)
+        UNION
+        (SELECT "accountNumber1" as "Jan", "accountNumber2"  as Friend FROM ASSOCIATED_ACCOUNTS )
+    ) associated
+ON  associated."Jan" = t1."accountNumber"
+) leftSide
+On leftSide."Friend" = rightside."accountNumber";
 
 
 
